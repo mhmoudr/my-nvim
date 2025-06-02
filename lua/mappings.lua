28433748fbaa8acad1cb1d6b197d97ebc94f7eff
Mahmoud Rawas <mhmoudr@gmail.com>
2025-06-02 20:45:30 +1000

    add key maping for code actions

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map({"n","i"}, "<A-cr>", function () vim.lsp.buf.code_action() end, {desc="Code actions"})
+map("n", "<leader>ca", function () vim.lsp.buf.code_action() end, {desc="Code actions"})
+map("i", "<C-c>a", function () vim.lsp.buf.code_action() end, {desc="Code actions"})
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
